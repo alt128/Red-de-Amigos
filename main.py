5472305efe5d535bdb42371d9f5d101f77cf0de4
Elmer Augusto Riva Rodriguez <67970179+alt128@users.noreply.github.com>
2024-06-09 21:15:57 -0700

    Update main.py. Función main que ejecuta la primera ventana (Inicio de sesión) creada

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import sys
+from PyQt5 import QtWidgets
+from inicio_sesionV import Ui_inicio_sesion
+
+class MainWindow(QtWidgets.QWidget, Ui_inicio_sesion):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        # Aquí puedes agregar tus conexiones de señales y slots
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
 
