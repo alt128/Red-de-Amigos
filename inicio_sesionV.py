f06e2554e065eb7ee8fcb4b873ace4cee90465cf
Elmer Augusto Riva Rodriguez <67970179+alt128@users.noreply.github.com>
2024-06-09 21:09:55 -0700

    inicio_sesionV.py actualizada. Interfaz grafica de Inicio de sesión implementada.

--- inicio_sesionV.py
+++ inicio_sesionV.py
@@ -1,1 +1,83 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_inicio_sesion(object):
+    def setupUi(self, inicio_sesion):
+        inicio_sesion.setObjectName("Inicio sesion")
+        inicio_sesion.resize(1300, 800)
+        inicio_sesion.setStyleSheet("background-color: rgb(10, 125, 208);")
+        self.btnCreateAccount = QtWidgets.QPushButton(inicio_sesion)
+        self.btnCreateAccount.setGeometry(QtCore.QRect(550, 590, 176, 35))
+        self.btnCreateAccount.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.btnCreateAccount.setStyleSheet("background-color: rgb(19, 44, 74);\n" "font: 12pt \"Inter\";\n" "color: rgb(255, 255, 255);\n" "border-color: rgb(10, 125, 208);")
+        self.btnCreateAccount.setObjectName("btnCreateAccount")
+        self.line_4 = QtWidgets.QFrame(inicio_sesion)
+        self.line_4.setGeometry(QtCore.QRect(1185, 660, 3, 61))
+        self.line_4.setFrameShape(QtWidgets.QFrame.VLine)
+        self.line_4.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line_4.setObjectName("line_4")
+        self.line = QtWidgets.QFrame(inicio_sesion)
+        self.line.setGeometry(QtCore.QRect(75, 40, 1111, 20))
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.titulo = QtWidgets.QLabel(inicio_sesion)
+        self.titulo.setGeometry(QtCore.QRect(310, 70, 670, 121))
+        self.titulo.setStyleSheet("font: 700 48pt \"Manrope\";\n" "color: rgb(255, 255, 255);")
+        self.titulo.setObjectName("titulo")
+        self.inputPassword = QtWidgets.QLineEdit(inicio_sesion)
+        self.inputPassword.setGeometry(QtCore.QRect(415, 440, 460, 41))
+        self.inputPassword.setStyleSheet("background-color: rgb(255, 255, 255);\n" "font: 14pt \"Segoe UI\";")
+        self.inputPassword.setObjectName("inputPassword")
+        self.inputUser = QtWidgets.QLineEdit(inicio_sesion)
+        self.inputUser.setGeometry(QtCore.QRect(415, 290, 460, 41))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.inputUser.sizePolicy().hasHeightForWidth())
+        self.inputUser.setSizePolicy(sizePolicy)
+        self.inputUser.setStyleSheet("background-color: rgb(255, 255, 255);\n" "font: 14pt \"Segoe UI\";")
+        self.inputUser.setObjectName("inputUser")
+        self.line_3 = QtWidgets.QFrame(inicio_sesion)
+        self.line_3.setGeometry(QtCore.QRect(75, 710, 1111, 20))
+        self.line_3.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line_3.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line_3.setObjectName("line_3")
+        self.user = QtWidgets.QLabel(inicio_sesion)
+        self.user.setGeometry(QtCore.QRect(415, 220, 281, 41))
+        self.user.setStyleSheet("font: 700 16pt \"Inter\";\n" "color: rgb(15, 34, 57);")
+        self.user.setObjectName("user")
+        self.line_2 = QtWidgets.QFrame(inicio_sesion)
+        self.line_2.setGeometry(QtCore.QRect(75, 50, 3, 61))
+        self.line_2.setFrameShape(QtWidgets.QFrame.VLine)
+        self.line_2.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line_2.setObjectName("line_2")
+        self.password = QtWidgets.QLabel(inicio_sesion)
+        self.password.setGeometry(QtCore.QRect(415, 370, 171, 41))
+        self.password.setStyleSheet("font: 700 16pt \"Inter\";\n" "color: rgb(15, 34, 57);")
+        self.password.setObjectName("password")
+        self.btnLogin = QtWidgets.QPushButton(inicio_sesion)
+        self.btnLogin.setGeometry(QtCore.QRect(550, 530, 176, 35))
+        self.btnLogin.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.btnLogin.setStyleSheet("background-color: rgb(19, 44, 74);\n" "font: 12pt \"Inter\";\n" "color: rgb(255, 255, 255);\n" "border-color: rgb(10, 125, 208);")
+        self.btnLogin.setObjectName("btnLogin")
+
+        self.retranslateUi(inicio_sesion)
+        QtCore.QMetaObject.connectSlotsByName(inicio_sesion)
+
+
+    def retranslateUi(self, inicio_sesion):
+        _translate = QtCore.QCoreApplication.translate
+        inicio_sesion.setWindowTitle(_translate("inicio_sesion", "inicio_sesion"))
+        self.btnCreateAccount.setText(_translate("inicio_sesion", "Crear cuenta"))
+        self.titulo.setText(_translate("inicio_sesion", "RED DE AMIGOS"))
+        self.user.setText(_translate("inicio_sesion", "Nombre de usuario:"))
+        self.password.setText(_translate("inicio_sesion", "Contraseña:"))
+        self.btnLogin.setText(_translate("inicio_sesion", "Iniciar Sesión"))
+
+
+class MainWindow(QtWidgets.QWidget, Ui_inicio_sesion):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
 
