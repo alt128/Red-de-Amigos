e106328cb272193de757fc6197d6400adec1987e
Williams <164354079+WiJeGo@users.noreply.github.com>
2024-06-14 09:21:09 -0500

    Update amigoClass.py. Clase Amigo actualizada

--- amigoClass.py
+++ amigoClass.py
@@ -1,1 +1,13 @@
- 
+ class Amigo:
+    def __init__(self, id, relacion):
+        self.id = id
+        self.relacion = relacion
+
+    def __repr__(self):
+        return f"Amigo(id={self.id}, relacion={self.relacion})"
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'relacion': self.relacion
+        }
