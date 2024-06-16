b5eec819a259aba811a7e68b69441d7dc69c1d8f
Williams <164354079+WiJeGo@users.noreply.github.com>
2024-06-15 20:37:13 -0500

    Update usuarioClass.py. Funcion agregar_usuario_a_json(usuario, filename) agregada
    Update usuarioClass.py. Funcion agregar_usuario_a_json(usuario, filename) agregada

--- usuarioClass.py
+++ usuarioClass.py
@@ -2,7 +2,7 @@ import json
 from amigoClass import Amigo
 
 class Usuario:
-    def __init__(self, id, nombre, amigos, contrasenia, comida_favorita, pelicula_favorita, lugar_favorito, hobby, fobia, carrera, numero_hermanos, ciclo, donde_vive, tiene_novia):
+    def _init_(self, id, nombre, amigos, contrasenia, comida_favorita, pelicula_favorita, lugar_favorito, hobby, fobia, carrera, numero_hermanos, ciclo, donde_vive, tiene_novia):
         self.id = id
         self.nombre = nombre
         self.amigos = amigos
@@ -18,7 +18,7 @@ class Usuario:
         self.donde_vive = donde_vive
         self.tiene_novia = tiene_novia
 
-    def __repr__(self):
+    def _repr_(self):
         return (f"Usuario(id={self.id}, nombre='{self.nombre}', amigos={self.amigos}, "
                 f"contrasenia='{self.contrasenia}', comida_favorita='{self.comida_favorita}', "
                 f"pelicula_favorita='{self.pelicula_favorita}', lugar_favorito='{self.lugar_favorito}', "
@@ -42,3 +42,17 @@ class Usuario:
             'donde_vive': self.donde_vive,
             'tiene_novia': self.tiene_novia
         }
+    
+def agregar_usuario_a_json(usuario, filename):
+    with open(filename, 'r+', encoding='utf-8') as file:
+        # Cargar los datos existentes
+        json_data = json.load(file)
+        
+        # Convertir el usuario a un diccionario y agregarlo a la lista de usuarios
+        nuevo_usuario = usuario.to_dict()
+        json_data['usuarios'].append(nuevo_usuario)
+        
+        # Volver al inicio del archivo y escribir los datos actualizados
+        file.seek(0)
+        json.dump(json_data, file, ensure_ascii=False, indent=4)
+        file.truncate()
