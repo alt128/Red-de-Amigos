495c2bead4740b40809c3a306a135d7a6fed5fd6
Williams <164354079+WiJeGo@users.noreply.github.com>
2024-06-19 20:16:19 -0500

    Update usuarioClass.py
    Update usuarioClass.py. He implementado los metodos eliminar_solicitud_amistad((), actualizar_amigos_json() y actualizar_solicitudes_amistad_json(). Que seran usadas especialmente por la clase Ui_cuestionario_relacion

--- usuarioClass.py
+++ usuarioClass.py
@@ -2,7 +2,7 @@ import json
 from amigoClass import Amigo
 
 class Usuario:
-    def __init__(self, id, nombre, amigos, contrasenia, comida_favorita, pelicula_favorita, lugar_favorito, hobby, fobia, carrera, numero_hermanos, ciclo, donde_vive, tiene_novia):
+    def _init_(self, id, nombre, amigos, contrasenia, comida_favorita, pelicula_favorita, lugar_favorito, hobby, fobia, carrera, numero_hermanos, ciclo, donde_vive, tiene_novia,solicitudes_amistad):
         self.id = id
         self.nombre = nombre
         self.amigos = amigos
@@ -17,14 +17,16 @@ class Usuario:
         self.ciclo = ciclo
         self.donde_vive = donde_vive
         self.tiene_novia = tiene_novia
+        self.solicitudes_amistad = solicitudes_amistad
 
-    def __repr__(self):
+    def _repr_(self):
         return (f"Usuario(id={self.id}, nombre='{self.nombre}', amigos={self.amigos}, "
                 f"contrasenia='{self.contrasenia}', comida_favorita='{self.comida_favorita}', "
                 f"pelicula_favorita='{self.pelicula_favorita}', lugar_favorito='{self.lugar_favorito}', "
                 f"hobby='{self.hobby}', fobia='{self.fobia}', carrera='{self.carrera}', "
                 f"numero_hermanos={self.numero_hermanos}, ciclo='{self.ciclo}', donde_vive='{self.donde_vive}', "
-                f"tiene_novia='{self.tiene_novia}')")
+                f"tiene_novia='{self.tiene_novia}', solicitudes_amistad={self.solicitudes_amistad})")
+    
     def to_dict(self):
         return {
             'id': self.id,
@@ -40,8 +42,53 @@ class Usuario:
             'numero_hermanos': self.numero_hermanos,
             'ciclo': self.ciclo,
             'donde_vive': self.donde_vive,
-            'tiene_novia': self.tiene_novia
+            'tiene_novia': self.tiene_novia,
+            'solicitudes_amistad': self.solicitudes_amistad
         }
+
+    def getNombre(self):
+        return self.nombre
+    
+    
+    def eliminar_solicitud_amistad(self, id, nombre_usuario, resultado_cuestionario, valor_relacion, filename):
+        solicitud = {
+            'id': int(id),
+            'nombre_usuario': str(nombre_usuario),
+            'resultado_cuestionario': int(resultado_cuestionario)
+        }
+        amigo = {
+            "id": id,
+            "relacion": valor_relacion,
+        }
+        if solicitud == self.solicitudes_amistad[0]:
+            print('SI IGUALITOS')
+        self.solicitudes_amistad.remove(solicitud)
+        self.actualizar_solicitudes_amistad_json(filename)
+        self.amigos.append(amigo)
+        self.actualizar_amigos_json(filename)
+
+
+    def actualizar_amigos_json(self, filename): #actualizar amigos
+        with open(filename, 'r+', encoding='utf-8') as file:
+            json_data = json.load(file)
+            for usuario in json_data['usuarios']:
+                if usuario['id'] == self.id:
+                    usuario['amigos'] = self.amigos
+                    break
+            file.seek(0)
+            json.dump(json_data, file, ensure_ascii=False, indent=4)
+            file.truncate()
+
+    def actualizar_solicitudes_amistad_json(self, filename): #actualiza solicitudes_amistad
+        with open(filename, 'r+', encoding='utf-8') as file:
+            json_data = json.load(file)
+            for usuario in json_data['usuarios']:
+                if usuario['id'] == self.id:
+                    usuario['solicitudes_amistad'] = self.solicitudes_amistad
+                    break
+            file.seek(0)
+            json.dump(json_data, file, ensure_ascii=False, indent=4)
+            file.truncate()
     
 def agregar_usuario_a_json(usuario, filename):
     with open(filename, 'r+', encoding='utf-8') as file:
@@ -55,4 +102,4 @@ def agregar_usuario_a_json(usuario, filename):
         # Volver al inicio del archivo y escribir los datos actualizados
         file.seek(0)
         json.dump(json_data, file, ensure_ascii=False, indent=4)
-        file.truncate()
+        file.truncate()
